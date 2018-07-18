47c5aec62104af5d9d6fd65b9c985722e4dae14a
vagrant <vagrant@p4>
2018-07-18 20:47:33 +0000

    updated rules for new topology / synthesis q

--- testing/multiswitch/writeRules.py
+++ testing/multiswitch/writeRules.py
@@ -15,8 +15,19 @@ import p4runtime_lib.bmv2
 from p4runtime_lib.switch import ShutdownAllSwitchConnections
 import p4runtime_lib.helper
 
-def writeForwarding(p4info_helper, sw, dst_eth_addr, dst_ip_addr, port):
+#need to deal with multiple switches? maybe provide ingress & eggress switch specifications
+#need to come up with something to remove forwarding rules
+
+def writeForwarding(p4info_helper, ingress_sw, egress_sw, tunnel_id,
+                     dst_eth_addr, dst_ip_addr):
     """
+    Installs three rules:
+    1) An tunnel ingress rule on the ingress switch in the ipv4_lpm table that
+       encapsulates traffic into a tunnel with the specified ID
+    2) A transit rule on the ingress switch that forwards traffic based on
+       the specified ID
+    3) An tunnel egress rule on the egress switch that decapsulates traffic
+       with the specified ID and sends it to the host
     :param p4info_helper: the P4Info helper
     :param ingress_sw: the ingress switch connection
     :param egress_sw: the egress switch connection
@@ -25,15 +36,53 @@ def writeForwarding(p4info_helper, sw, dst_eth_addr, dst_ip_addr, port):
     :param dst_ip_addr: the destination Ethernet address to write in the
                         egress rule
     """
+    # 1) Tunnel Ingress Rule
     table_entry = p4info_helper.buildTableEntry(
         table_name="MyIngress.ipv4_lpm",
         match_fields={
             "hdr.ipv4.dstAddr": (dst_ip_addr, 32)
         },
-        action_name="MyIngress.ipv4_forward",
+        action_name="MyIngress.myTunnel_ingress",
         action_params={
-            "port": port,
-            "dstAddr": dst_eth_addr
+            "dst_id": tunnel_id,
         })
-    sw.WriteTableEntry(table_entry)
-    print "Installed transit rule on %s" % sw.name
+    ingress_sw.WriteTableEntry(table_entry)
+    print "Installed ingress tunnel rule on %s" % ingress_sw.name
+
+    # 2) Tunnel Transit Rule
+    # The rule will need to be added to the myTunnel_exact table and match on
+    # the tunnel ID (hdr.myTunnel.dst_id). Traffic will need to be forwarded
+    # using the myTunnel_forward action on the port connected to the next switch.
+    #
+    # For our simple topology, switch 1 and switch 2 are connected using a
+    # link attached to port 2 on both switches. We have defined a variable at
+    # the top of the file, SWITCH_TO_SWITCH_PORT, that you can use as the output
+    # port for this action.
+    #
+    # We will only need a transit rule on the ingress switch because we are
+    # using a simple topology. In general, you'll need on transit rule for
+    # each switch in the path (except the last switch, which has the egress rule),
+    # and you will need to select the port dynamically for each switch based on
+    # your topology.
+
+    # TODO build the transit rule
+    # TODO install the transit rule on the ingress switch
+    print "TODO Install transit tunnel rule"
+
+    # 3) Tunnel Egress Rule
+    # For our simple topology, the host will always be located on the
+    # SWITCH_TO_HOST_PORT (port 1).
+    # In general, you will need to keep track of which port the host is
+    # connected to.
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="MyIngress.myTunnel_exact",
+        match_fields={
+            "hdr.myTunnel.dst_id": tunnel_id
+        },
+        action_name="MyIngress.myTunnel_egress",
+        action_params={
+            "dstAddr": dst_eth_addr,
+            "port": SWITCH_TO_HOST_PORT
+        })
+    egress_sw.WriteTableEntry(table_entry)
+    print "Installed egress tunnel rule on %s" % egress_sw.name
